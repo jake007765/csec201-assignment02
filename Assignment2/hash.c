83f474f8947c609e7818ebd462be3cf4a8054f33
Jake Streamer <jws8495@rit.edu>
2023-10-11 21:48:44 -0400

    part a is about to get fuckin real

--- Assignment2/hash.c
+++ Assignment2/hash.c
@@ -1,46 +1,53 @@
-#include "hash.h"
-
-//SHA_40 Digest
-struct Digest {
-	BYTE hash0, hash1, hash2, hash3, hash4;
-};
+﻿#include "hash.h"
+#include <stdlib.h>
 
 
-//SHA_40 hash function
-unsigned char* SHA_40(const unsigned char* x, size_t size)
-{
-	unsigned char A = 11;
-	unsigned char B = 22;
-	unsigned char C = 33;
-	unsigned char D = 44;
-	unsigned char E = 55;
-
-	for (int i = 0; i < size; i++)
-	{
-		for (int r = 0; r < 12; r++) {
-			unsigned char F = (B & C) ^ D;
-			unsigned char newA = A;
-			/*Use the diagram to determine what these values should be*/
-			A = ?????
-			E = ?????
-			D = ?????
-			C = ?????
-			B = ?????
-		}
-	}
-
-	unsigned char* digest = (unsigned char*)malloc(DIGEST_SIZE);
-
-	digest[0] = A;
-	digest[1] = B;
-	digest[2] = C;
-	digest[3] = D;
-	digest[4] = E;
-
-	return digest;
+// Helper function for circular left shift
+unsigned char rotateLeft(unsigned char value, int shift) {
+    return (value << shift) | (value >> (8 - shift));
 }
 
-//return one (true) or return zero (false)
+// SHA-40 hash function
+unsigned char* SHA_40(const unsigned char* x, size_t size, struct Digest* digest) {
+    unsigned char A = 11;
+    unsigned char B = 22;
+    unsigned char C = 33;
+    unsigned char D = 44;
+    unsigned char E = 55;
+
+    for (size_t i = 0; i < size; i++) {
+        for (int r = 0; r < 12; r++) {
+            unsigned char F = (B & C) ^ D;
+            unsigned char newA = A;
+
+            A = rotateLeft(A, 5) ^ F;
+            B = B ^ C;
+            C = rotateLeft(D, 2) ^ E;
+            D = D ^ A;
+            E = E ^ newA;
+        }
+    }
+
+    // STOP! YOU VIOLATED THE LAW! PAY THE COURT A FINE OR SERVE YOUR SENTENCE, YOUR STOLEN STRUCTS ARE NOW FORFEIT.﻿ 
+
+    digest->hash0 = A;
+    digest->hash1 = B;
+    digest->hash2 = C;
+    digest->hash3 = D;
+    digest->hash4 = E;
+}
+
+
+
 int digestEqual(struct Digest* digest1, struct Digest* digest2) {
-	//Should return 1 if the two digests are equal and 0 if the two digests are not equal.
+    return (digest1->hash0 == digest2->hash0 &&
+        digest1->hash1 == digest2->hash1 &&
+        digest1->hash2 == digest2->hash2 &&
+        digest1->hash3 == digest2->hash3 &&
+        digest1->hash4 == digest2->hash4);
+}
+
+// Define the printDigest function
+void printDigest(struct Digest* digest) {
+    printf("%02X %02X %02X %02X %02X\n", digest->hash0, digest->hash1, digest->hash2, digest->hash3, digest->hash4);
 }
--- Assignment2/main.c
+++ Assignment2/main.c
@@ -1,6 +1,65 @@
 #include <stdio.h>
+#include <string.h>
 #include "hash.h"
 
-int main(void) {
+int main() {
+    // PART A
 
+    // Test SHA-40 hash of strings
+    const char* string1 = "Rob";
+    const char* string2 = "James";
+    const char* string3 = "Ahmed";
+    const char* string4 = "CSEC";
+    const char* string5 = "Jake";
+
+    // Declare arrays to store hash values
+    unsigned char hash1[DIGEST_SIZE];
+    unsigned char hash2[DIGEST_SIZE];
+    unsigned char hash3[DIGEST_SIZE];
+    unsigned char hash4[DIGEST_SIZE];
+    unsigned char hash5[DIGEST_SIZE];
+
+    // Calculate SHA-40 hashes and store them in the arrays
+    SHA_40((const unsigned char*)string1, strlen(string1), hash1);
+    SHA_40((const unsigned char*)string2, strlen(string2), hash2);
+    SHA_40((const unsigned char*)string3, strlen(string3), hash3);
+    SHA_40((const unsigned char*)string4, strlen(string4), hash4);
+    SHA_40((const unsigned char*)string5, strlen(string5), hash5);
+
+    // Print SHA-40 hashes
+    printf("SHA-40 Hash of 'Rob': ");
+    for (int i = 0; i < DIGEST_SIZE; i++) {
+        printf("%02x", hash1[i]);
+    }
+    printf("\n");
+
+    printf("SHA-40 Hash of 'James': ");
+    for (int i = 0; i < DIGEST_SIZE; i++) {
+        printf("%02x", hash2[i]);
+    }
+    printf("\n");
+
+    printf("SHA-40 Hash of 'Ahmed': ");
+    for (int i = 0; i < DIGEST_SIZE; i++) {
+        printf("%02x", hash3[i]);
+    }
+    printf("\n");
+
+    printf("SHA-40 Hash of 'CSEC': ");
+    for (int i = 0; i < DIGEST_SIZE; i++) {
+        printf("%02x", hash4[i]);
+    }
+    printf("\n");
+
+    printf("SHA-40 Hash of 'Jake': ");
+    for (int i = 0; i < DIGEST_SIZE; i++) {
+        printf("%02x", hash5[i]);
+    }
+    printf("\n");
+
+    // PART B
+
+    // PART C
+
+    return 0;
 }
